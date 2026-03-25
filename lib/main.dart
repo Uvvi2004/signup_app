6be8915ed95c54d51b989d3f143bacf595f04c42
Uddhav <uddhav3114@gmail.com>
2026-03-25 18:24:24 -0400

    Create a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -141,3 +141,25 @@ class _SignupPageState extends State<SignupPage> {
     );
   }
 }
+
+class WelcomeScreen extends StatelessWidget {
+  final String name;
+
+  const WelcomeScreen({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome Screen'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: Text(
+          'Welcome, $name!',
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
